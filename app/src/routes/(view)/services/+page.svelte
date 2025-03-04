847c32a16707b945128b113d0a811e55f1261cc8
Hacks-coder <ajheartcry@gmail.com>
2025-03-03 23:32:34 -0800

    added the linked pages

---
+++ app/src/routes/(view)/services/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import One from "../../../components/Services/One.svelte";
+	import Three from "../../../components/Services/Three.svelte";
+	import Two from "../../../components/Services/Two.svelte";
+
+</script>
+<One />
+<Two />
+<Three />
